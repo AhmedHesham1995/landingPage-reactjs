cbe0b414742679713490d2ea88526ec50324772c
AhmedHesham1995 <ahmed.hesham.ahmed10@gmail.com>
2023-06-22 19:19:33 +0300

    last

--- src/components/Nav/Navbars.js
+++ src/components/Nav/Navbars.js
@@ -2,19 +2,25 @@ import { Navbar,Container,Nav } from "react-bootstrap";
 import './Navbars.css';
 import { NavLink } from "react-router-dom";
 function Navbars(){
+    const handleNavClick=()=>{
+        window.scrollTo({
+            top:0,
+            behavior:'smooth'
+        })
+    }
     return(
         <Navbar expand="lg" fixed="top">
             <Container>
-                <Navbar.Brand href="/" className="logo">Dorsin</Navbar.Brand>
+                <Navbar.Brand href="/" className="logo"  onClick={handleNavClick}>Dorsin</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <NavLink to="/" className="first">Home</NavLink>
-                    <NavLink to="/services">Services</NavLink>
-                    <NavLink to="/pricing">Pricing</NavLink>
-                    <NavLink to="/team">Team</NavLink>
-                    <NavLink to="/blog">Blog</NavLink>
-                    <NavLink to="/contact">Contact</NavLink>
+                    <NavLink to="/" className="first" onClick={handleNavClick}>Home</NavLink>
+                    <NavLink to="/services" onClick={handleNavClick}>Services</NavLink>
+                    <NavLink to="/pricing" onClick={handleNavClick}>Pricing</NavLink>
+                    <NavLink to="/team" onClick={handleNavClick}>Team</NavLink>
+                    <NavLink to="/blog" onClick={handleNavClick}>Blog</NavLink>
+                    <NavLink to="/contact" onClick={handleNavClick}>Contact</NavLink>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
